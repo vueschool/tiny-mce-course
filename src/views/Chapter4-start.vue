df7cd68fea0ab922f898e3c90be3823b624004bb
Daniel Kelly <me@danielkelly.io>
2025-01-15 17:05:09 -0600

    14-prevent-loosing-unsaved-changes-with-the-autosave-plugin

--- src/views/Chapter4-start.vue
+++ src/views/Chapter4-start.vue
@@ -4,9 +4,10 @@ import { ref } from 'vue'
 import Editor from '@tinymce/tinymce-vue'
 import page from '@/cms-pages/index.html?raw'
 import css from '@/cms-pages/style.css?raw'
+import { useLocalStorage } from '@vueuse/core'
 
 const apiKey = import.meta.env.VITE_API_KEY
-const value = ref(page)
+const value = useLocalStorage('tinymce-autosave-my-cms-id', page)
 </script>
 <template>
   <PageHeading class="flex justify-between w-[90%]">
@@ -14,11 +15,13 @@ const value = ref(page)
   </PageHeading>
 
   <div>
-    <Editor :api-key="apiKey" :style="{
+    <Editor :api-key="apiKey" id="my-cms-id" :style="{
       height: 'calc(100vh - 100px)',
       width: '90%'
     }" :init="{
-      plugins: 'lists link image table help anchor',
+      plugins: 'lists link image table help anchor autosave save',
+      autosave_interval: '10s',
+      autosave_prefix: 'tinymce-autosave-{id}',
       style_formats: [
         { title: 'Paragraph', block: 'p' },
         { title: 'Heading 1', block: 'h1' },
@@ -39,8 +42,11 @@ const value = ref(page)
           }
         }
       },
+      save_onsavecallback: () => {
+        console.log('Saved!')
+      },
       toolbar:
-        'styles anchor italic bold underline strikethrough | bullist numlist | link image table code help',
+        'save styles anchor italic bold underline strikethrough | bullist numlist | link image table code help',
       content_style: css,
       content_css: 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css',
       editable_root: false,
