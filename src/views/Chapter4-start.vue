4f213839565570b3fc3f48ec8711308809e1e9d5
Daniel Kelly <me@danielkelly.io>
2025-01-16 12:51:11 -0600

    16-pro-tinymce-features-perfectly-fit-for-a-cms

--- src/views/Chapter4-start.vue
+++ src/views/Chapter4-start.vue
@@ -5,6 +5,9 @@ import Editor from '@tinymce/tinymce-vue'
 import page from '@/cms-pages/index.html?raw'
 import css from '@/cms-pages/style.css?raw'
 import { useLocalStorage } from '@vueuse/core'
+import UserPicker from '@/components/UserPicker.vue'
+
+const user = ref({ username: 'danielkellyio', name: 'Daniel Kelly', avatar: 'https://avatars.githubusercontent.com/u/7635209?v=4' });
 
 const apiKey = import.meta.env.VITE_API_KEY
 const value = useLocalStorage('tinymce-autosave-my-cms-id', page)
@@ -12,14 +15,15 @@ const value = useLocalStorage('tinymce-autosave-my-cms-id', page)
 <template>
   <PageHeading class="flex justify-between w-[90%]">
     <span>Use TinyMCE as a CMS</span>
+    <UserPicker v-model="user"></UserPicker>
   </PageHeading>
 
-  <div>
+  <div :key="user.username">
     <Editor :api-key="apiKey" id="my-cms-id" :style="{
       height: 'calc(100vh - 100px)',
       width: '90%'
     }" :init="{
-      plugins: 'lists link image table help anchor autosave save advcode',
+      plugins: 'lists link image table help anchor autosave save advcode tinycomments',
       autosave_interval: '10s',
       autosave_prefix: 'tinymce-autosave-{id}',
       style_formats: [
@@ -47,7 +51,11 @@ const value = useLocalStorage('tinymce-autosave-my-cms-id', page)
       },
       advcode_inline: true,
       toolbar:
-        'save styles anchor italic bold underline strikethrough | bullist numlist | link image table help code',
+        'save styles anchor italic bold underline strikethrough | bullist numlist | link image table help code showcomments addcomment',
+      tinycomments_mode: 'embedded',
+      tinycomments_author: user.username,
+      tinycomments_author_name: user.name,
+      tinycomments_author_avatar: user.avatar,
       content_style: css,
       content_css: 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css',
       editable_root: false,
